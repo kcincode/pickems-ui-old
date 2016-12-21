ce89024e914952f39478033490be57abf4ebffa7
Nick Felicelli <nick.felicelli@gmail.com>
2016-12-20 20:48:40 -0500

    Fixing tests

--- tests/unit/models/admin-team-test.js
+++ tests/unit/models/admin-team-test.js
@@ -2,7 +2,7 @@ import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('admin-team', 'Unit | Model | admin team', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: ['model:user']
 });
 
 test('it exists', function(assert) {
