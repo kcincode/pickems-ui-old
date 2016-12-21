aa701e8de902a6611ebff99bdd5b49f7b350f277
Nick Felicelli <nick.felicelli@gmail.com>
2016-12-20 20:59:44 -0500

    Restoring eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,16 @@ module.exports = {
     ecmaVersion: 6,
     sourceType: 'module'
   },
-  extends: 'eslint:recommended',
+  extends: [
+    'eslint:recommended',
+    'plugin:ember-suave/recommended'
+  ],
   env: {
     'browser': true
   },
   rules: {
+    'camelcase': 0,
+    'ember-suave/require-access-in-comments': 0,
+    'ember-suave/no-direct-property-access': 0
   }
 };
