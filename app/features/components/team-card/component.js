884be4274c983fa81c427b57b20ae644dc0ea94a
Nick Felicelli <nick.felicelli@gmail.com>
2017-03-14 10:46:07 -0400

    Implementing the playoff rankings and the detail picks display

--- app/features/components/team-card/component.js
+++ app/features/components/team-card/component.js
@@ -7,6 +7,7 @@ export default Ember.Component.extend({
   system: service(),
   changeWeek: 'changeWeek',
   showWeeks: true,
+  playoff: false,
 
   actions: {
     changeWeek(week) {
--- app/features/teams/playoff-picks/route.js
+++ app/features/teams/playoff-picks/route.js
@@ -1,4 +1,37 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import ENV from 'pickems/config/environment';
 
-export default Ember.Route.extend({
+const { RSVP, inject: { service } } = Ember;
+
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  system: service(),
+
+  model(params) {
+    // generate the picks urls
+    let picksUrl = `${ENV.api.host}/${ENV.api.namespace}/playoff-picks/${params.team}/details`;
+
+    return RSVP.hash({
+      team: this.store.query('team', { slug: params.team }).then((teams) => {
+        return teams.get('firstObject');
+      }),
+      teamPicks: fetch(picksUrl, {
+        type: 'GET',
+        headers: {
+          'Authorization': `${ENV.api.headerKey} ${this.get('session').get('session.content.authenticated.access_token')}`
+        }
+      }).then((response) => response.json())
+    });
+  },
+
+  setupController(controller) {
+    this._super(...arguments);
+
+    controller.set('system', this.get('system'));
+    this.controllerFor('teams').set('showTeamSelect', false);
+  },
+
+  resetController() {
+    this.controllerFor('teams').set('showTeamSelect', true);
+  }
 });
